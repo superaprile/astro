736caddcea466e02580528b3f6913afbc0bb0158
superaprile <ret-igugale23@tutamail.com>
2025-02-24 16:31:46 +0100

    adde preact package to the project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 export default defineConfig({
   site: "https://testing-astro.netlify.app/",
+  integrations: [preact()],
 });
